75fbb854eb5325c8b2386cdaa1c685478cb0b0b1
unknown <370724104@qq.com>
2025-11-25 23:37:57 +0800

    任务3 提交

--- go/Task3/GORM/models/models.go
+++ go/Task3/GORM/models/models.go
@@ -6,16 +6,19 @@ import (
 
 type User struct {
 	gorm.Model
-	Name     string
-	Posts    []Post
-	Comments []Comment
+	Name      string
+	Posts     []Post
+	Comments  []Comment
+	PostCount int
 }
 type Post struct {
 	gorm.Model
-	Title    string
-	Content  string
-	UserId   uint
-	Comments []Comment
+	Title         string
+	Content       string
+	UserId        uint
+	Comments      []Comment
+	CommentCount  int
+	CommentStatus string `gorm:"default:'无评论'"`
 }
 type Comment struct {
 	gorm.Model
@@ -24,4 +27,30 @@ type Comment struct {
 	Post    Post
 	UserId  uint
 	User    User
+}
+
+func (p *Post) AfterCreate(tx *gorm.DB) (err error) {
+	var user User
+	tx.First(&user, p.UserId)
+	user.PostCount += 1
+	tx.Model(&user).Update("post_count", user.PostCount)
+	return
+}
+func (c *Comment) AfterCreate(tx *gorm.DB) (err error) {
+	var post Post
+	tx.First(&post, c.PostId)
+	post.CommentCount += 1
+	post.CommentStatus = "有评论"
+	tx.Model(&post).Select("CommentCount", "CommentStatus").Updates(&post)
+	return
+}
+func (c *Comment) AfterDelete(tx *gorm.DB) (err error) {
+	var post Post
+	tx.First(&post, c.PostId)
+	post.CommentCount -= 1
+	if post.CommentCount == 0 {
+		post.CommentStatus = "无评论"
+	}
+	tx.Model(&post).Select("CommentCount", "CommentStatus").Updates(&post)
+	return
 }
